6285a8b9ab2be98fdbfa9f2ebac0fa3e3cda4b92
Emeline <emeline.courcelle@gmail.com>
2024-01-26 11:01:44 +0100

    h

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("vercelexo-backend.vercel.app/year").then(response => response.json()).then(data => {
+fetch("https://vercelexo-backend.vercel.app/year").then(response => response.json()).then(data => {
     console.log(data.year)
     document.querySelector("#year").textContent = data.year
 })
