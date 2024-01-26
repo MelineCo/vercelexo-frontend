f6e6480d215e4c27fcf2a34f941e5483ff2f5277
Emeline <emeline.courcelle@gmail.com>
2024-01-26 10:58:26 +0100

    modfy fetch url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:3000/year").then(response => response.json()).then(data => {
+fetch("vercelexo-backend.vercel.app/year").then(response => response.json()).then(data => {
     console.log(data.year)
     document.querySelector("#year").textContent = data.year
 })
